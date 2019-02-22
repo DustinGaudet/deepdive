37d8bacc696b26ece0059a3b7dd7ecf1a3b616c0
DustinGaudet <dustin@dustingaudet.com>
2019-02-22 15:04:50 -0500

    Prevent task completion click event from bubbling up

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -9,24 +9,25 @@ class Task extends Component {
   handleSingleOrDoubleClick = (e) => {
 
     e.preventDefault()
+    if (e.target === e.currentTarget) {
+      this.setState({timer: 1})
 
-    this.setState({timer: 1})
-
-    console.log(this.state.timer)    
-
-    if (this.state.timer) {
-      this.props.handleDoubleClickTask()
-      // clearTimeout(timer)
-      clearTimeout(this.state.timer)
-      this.setState({timer: 0})
-      console.log(this.state.timer)
-    } else {
-      this.props.handleSingleClickTask(this.props.task)
-      const intervalId = setTimeout(() => {
+      console.log(this.state.timer)    
+  
+      if (this.state.timer) {
+        this.props.handleDoubleClickTask()
+        // clearTimeout(timer)
         clearTimeout(this.state.timer)
         this.setState({timer: 0})
-      }, 200)
-      this.setState({timer: intervalId})
+        console.log(this.state.timer)
+      } else {
+        this.props.handleSingleClickTask(this.props.task)
+        const intervalId = setTimeout(() => {
+          clearTimeout(this.state.timer)
+          this.setState({timer: 0})
+        }, 200)
+        this.setState({timer: intervalId})
+      }
     }
   }
 
